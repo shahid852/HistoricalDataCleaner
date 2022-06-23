65e3e328538cc5119dd5123e249c059649fbfc7a
shahid852 <40851140+shahid852@users.noreply.github.com>
2022-06-23 17:47:03 +0500

    Initial release - Data cleaning 1. Unix data converted to long 2. Date format uniform 3. Duplicate date records removed

--- HistoricalDataCleaner/Form1.Designer.cs
+++ HistoricalDataCleaner/Form1.Designer.cs
@@ -32,17 +32,24 @@
             this.dlgSrcFolder = new System.Windows.Forms.FolderBrowserDialog();
             this.chkListInputFiles = new System.Windows.Forms.CheckedListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblFolderPath = new System.Windows.Forms.Label();
+            this.lblInputFolderPath = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblOutputFolderPath = new System.Windows.Forms.Label();
+            this.chkListOutputFiles = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btcStartCleaning = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSrcFolderBrowse
             // 
-            this.btnSrcFolderBrowse.Location = new System.Drawing.Point(489, 32);
+            this.btnSrcFolderBrowse.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSrcFolderBrowse.Location = new System.Drawing.Point(539, 32);
             this.btnSrcFolderBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSrcFolderBrowse.Name = "btnSrcFolderBrowse";
-            this.btnSrcFolderBrowse.Size = new System.Drawing.Size(109, 38);
+            this.btnSrcFolderBrowse.Size = new System.Drawing.Size(59, 38);
             this.btnSrcFolderBrowse.TabIndex = 0;
             this.btnSrcFolderBrowse.Text = "...";
             this.btnSrcFolderBrowse.UseVisualStyleBackColor = true;
@@ -62,7 +69,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.lblFolderPath);
+            this.groupBox1.Controls.Add(this.lblInputFolderPath);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.chkListInputFiles);
             this.groupBox1.Controls.Add(this.btnSrcFolderBrowse);
@@ -74,14 +81,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Inputs";
             // 
-            // lblFolderPath
+            // lblInputFolderPath
             // 
-            this.lblFolderPath.AutoSize = true;
-            this.lblFolderPath.Location = new System.Drawing.Point(73, 42);
-            this.lblFolderPath.Name = "lblFolderPath";
-            this.lblFolderPath.Size = new System.Drawing.Size(157, 25);
-            this.lblFolderPath.TabIndex = 2;
-            this.lblFolderPath.Text = "No folder selected";
+            this.lblInputFolderPath.AutoSize = true;
+            this.lblInputFolderPath.Location = new System.Drawing.Point(73, 42);
+            this.lblInputFolderPath.Name = "lblInputFolderPath";
+            this.lblInputFolderPath.Size = new System.Drawing.Size(157, 25);
+            this.lblInputFolderPath.TabIndex = 2;
+            this.lblInputFolderPath.Text = "No folder selected";
             // 
             // label1
             // 
@@ -93,17 +100,72 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Folder:";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblOutputFolderPath);
+            this.groupBox2.Controls.Add(this.chkListOutputFiles);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btcStartCleaning);
+            this.groupBox2.Location = new System.Drawing.Point(623, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(811, 434);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cleaning";
+            // 
+            // lblOutputFolderPath
+            // 
+            this.lblOutputFolderPath.AutoSize = true;
+            this.lblOutputFolderPath.Location = new System.Drawing.Point(74, 43);
+            this.lblOutputFolderPath.Name = "lblOutputFolderPath";
+            this.lblOutputFolderPath.Size = new System.Drawing.Size(157, 25);
+            this.lblOutputFolderPath.TabIndex = 2;
+            this.lblOutputFolderPath.Text = "No folder selected";
+            // 
+            // chkListOutputFiles
+            // 
+            this.chkListOutputFiles.FormattingEnabled = true;
+            this.chkListOutputFiles.Location = new System.Drawing.Point(6, 70);
+            this.chkListOutputFiles.Name = "chkListOutputFiles";
+            this.chkListOutputFiles.Size = new System.Drawing.Size(799, 340);
+            this.chkListOutputFiles.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Folder:";
+            // 
+            // btcStartCleaning
+            // 
+            this.btcStartCleaning.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btcStartCleaning.Location = new System.Drawing.Point(655, 32);
+            this.btcStartCleaning.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btcStartCleaning.Name = "btcStartCleaning";
+            this.btcStartCleaning.Size = new System.Drawing.Size(149, 38);
+            this.btcStartCleaning.TabIndex = 0;
+            this.btcStartCleaning.Text = "Start";
+            this.btcStartCleaning.UseVisualStyleBackColor = true;
+            this.btcStartCleaning.Click += new System.EventHandler(this.btcStartCleaning_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1657, 714);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,7 +176,12 @@
         private FolderBrowserDialog dlgSrcFolder;
         private CheckedListBox chkListInputFiles;
         private GroupBox groupBox1;
-        private Label lblFolderPath;
+        private Label lblInputFolderPath;
         private Label label1;
+        private GroupBox groupBox2;
+        private CheckedListBox chkListOutputFiles;
+        private Button btcStartCleaning;
+        private Label lblOutputFolderPath;
+        private Label label2;
     }
 }
--- HistoricalDataCleaner/Form1.cs
+++ HistoricalDataCleaner/Form1.cs
@@ -1,4 +1,6 @@
 using CsvHelper;
+using System;
+using System.Data;
 using System.Globalization;
 
 namespace HistoricalDataCleaner
@@ -29,59 +31,163 @@ namespace HistoricalDataCleaner
             if (dlgSrcFolder.ShowDialog() == DialogResult.OK)
             {
                 string srcFolder = dlgSrcFolder.SelectedPath;
-                lblFolderPath.Text = srcFolder;
+                lblInputFolderPath.Text = srcFolder;
                 var folder = new DirectoryInfo(srcFolder);
                 var srcCsvFilePaths = folder.GetFiles("*.csv", SearchOption.TopDirectoryOnly);
-                Directory.CreateDirectory(srcFolder + "\\Outputs");
+
                 chkListInputFiles.Items.Clear();
                 foreach (FileInfo s in srcCsvFilePaths)
                 {
-                    if (s.Name.EndsWith("-output.csv")) continue; //skip the output files
+                    //if (s.Name.EndsWith("-output.csv")) continue; //skip the output files
                     chkListInputFiles.Items.Add(s.Name, true);
 
                 }
 
-                ////var config = new CsvHelper.Configuration.CsvConfiguration { };
-                ////config.
 
-
-                //foreach (FileInfo s in srcCsvFilePaths)
-                //{
-                //    if (s.Name.EndsWith("-output.csv")) continue; //skip the output files
-                //    //StreamReader sr = new StreamReader(s.FullName);
-                //    StreamWriter sw = new StreamWriter(s.FullName.Replace(".csv", "") + "-output.csv");
-
-                //    var sr = new StreamReader(s.FullName);
-                //    //sr.Peek();
-                //    string? line = sr.ReadLine();
-                //    while (String.IsNullOrEmpty(line)) line = sr.ReadLine();
-
-                //    if (!line.Contains(",")) line = sr.ReadLine(); // skip 1st line
-                //    using (var csv = new CsvReader(sr, CultureInfo.InvariantCulture))
-                //    {
-                //        var records = csv.GetRecords<record>();
-
-                //    }
-
-                //    //while (sr.Peek() >= 0)
-                //    //{
-                //    //    //perform task related to file 
-                //    //    string? line = sr.ReadLine();
-                //    //    if (line == null) continue;
-                //    //    if (!line.Contains(",")) continue;// skip 1st line
-
-
-                //    //    sw.WriteLine(line);
-
-
-
-
-                //    //}
-                //    sr.Close();
-                //    sw.Close();
-                //}
             }
 
         }
+        public static void ToCSV(DataTable dtDataTable, string strFilePath)
+        {
+            StreamWriter sw = new StreamWriter(strFilePath, false);
+            //headers    
+            for (int i = 0; i < dtDataTable.Columns.Count; i++)
+            {
+                sw.Write(dtDataTable.Columns[i]);
+                if (i < dtDataTable.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in dtDataTable.Rows)
+            {
+                for (int i = 0; i < dtDataTable.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < dtDataTable.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+        }
+
+        private void btcStartCleaning_Click(object sender, EventArgs e)
+        {
+            string tempFolder = dlgSrcFolder.SelectedPath + "\\Temp";
+            string destFolder = dlgSrcFolder.SelectedPath + "\\Outputs";
+            lblOutputFolderPath.Text = destFolder;
+            Directory.CreateDirectory(tempFolder);
+            Directory.CreateDirectory(destFolder);
+            //var config = new CsvHelper.Configuration.CsvConfiguration { };
+            //config.
+
+            for (int i = 0; i < chkListInputFiles.Items.Count; i++)
+            {
+                CheckState st = chkListInputFiles.GetItemCheckState(i);
+                if (st == CheckState.Unchecked)
+                    continue;
+
+                string fileName = chkListInputFiles.GetItemText(i);
+                FileInfo f_input = new FileInfo(dlgSrcFolder.SelectedPath + "\\" + chkListInputFiles.Items[i].ToString());
+                FileInfo f_temp = new FileInfo(tempFolder + "\\" + chkListInputFiles.Items[i].ToString().Replace(".csv", "") + "-temp.csv"); // Intermediate version
+                FileInfo f_output = new FileInfo(destFolder + "\\" + chkListInputFiles.Items[i].ToString().Replace(".csv", "") + "-output.csv"); // Final version
+
+                StreamReader sr_input = new StreamReader(f_input.FullName);
+                StreamWriter sw_temp = new StreamWriter(f_temp.FullName);
+
+                while (sr_input.Peek() >= 0)
+                {
+                    string? line_in = sr_input.ReadLine();
+                    if (String.IsNullOrEmpty(line_in)) continue; // skip blank lines
+                    if (!line_in.Contains(",")) continue;// skip non comma lines
+                    string line_out = line_in;
+                    ///////////////////////////////////////////////////////////
+                    // make the dates format consistent
+                    string date_orig = line_out.Split(",")[1];
+                    line_out = line_out.Replace(date_orig, "fillnewdatehere");
+                    string date_out = date_orig.Replace("-AM", ":00:00");
+                    if (date_orig.Contains("-PM"))
+                    {
+                        date_out = date_orig.Replace("-PM", ":00:00");
+                        DateTime outDt = DateTime.Parse(date_out).AddHours(12);
+                        date_out = outDt.ToString("yyyy-MM-dd HH:mm:ss");
+                    }
+                    line_out = line_out.Replace("fillnewdatehere", date_out);
+                    ////////////////////////////////////////////////////////////
+
+                    // make the tradecount format consistent
+                    string tradecount_orig = line_out.Split(",")[9];
+                    line_out = line_out.Replace("NULL", "0");
+                    ///////////////////////////////////////////////////////////
+
+                    /// make the unix date format consistent
+                    string unixdate = line_out.Split(",")[0];
+                    line_out = line_out.Replace(".0", "");
+                    ///////////////////////////////////////////////////////////
+
+                    sw_temp.WriteLine(line_out);
+                }
+                //sr_input.Close();
+                sw_temp.Close();
+
+                // After initial cleaning pass on to csv parser for data conversions
+
+                var dt = new DataTable();
+
+                using (var sr_temp = new StreamReader(f_temp.FullName))
+                using (var csv = new CsvReader(sr_temp, CultureInfo.InvariantCulture))
+                {
+                    // Do any configuration to `CsvReader` before creating CsvDataReader.
+                    using (var dr = new CsvDataReader(csv))
+                    {
+
+                        dt.Columns.Add("unix", typeof(long));
+                        dt.Columns.Add("Date", typeof(DateTime));
+                        dt.Columns.Add("symbol", typeof(string));
+                        dt.Columns.Add("open", typeof(float));
+                        dt.Columns.Add("high", typeof(float));
+                        dt.Columns.Add("low", typeof(float));
+                        dt.Columns.Add("close", typeof(float));
+                        dt.Columns.Add("Volume BTC", typeof(decimal));
+                        dt.Columns.Add("Volume USDT", typeof(decimal));
+                        dt.Columns.Add("tradecount", typeof(long));
+
+                        dt.Load(dr);
+                    }
+                }
+
+                // var finalDt = dt.DefaultView.ToTable(true, "Date");
+                //var finalDt = dt.AsEnumerable()
+                // .GroupBy(x => x.Field<int>("ColumnName"))
+                // .Select(y => y.First())
+                // .CopyToDataTable();
+                DataTable distinctDT = (from rows in dt.AsEnumerable()
+                                        group rows by new { ColA = rows["Date"] } into grp
+                                        select grp.First()).CopyToDataTable();
+
+                ToCSV(distinctDT, f_output.FullName);
+                chkListOutputFiles.Items.Add(f_output.Name, true);
+            }
+
+        }
+
     }
+
+
 }
